5a66ae9c37b6375deea008702b96e030516ee203
jungchungsub <cndtjq1248@naver.com>
2023-03-26 18:31:47 +0900

    색상추가

---
+++ lib/constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const gMainColor = Color(0xFFB275E8);
+const gSecondColor = Color(0xFFDABFF5);
+const gThirdColor = Color(0xFFE8D0FF);
+const gButtonColor = Color(0xFFBC70FF);
